2190838390b8970c0e68ecba6aaba5191df94609
asdfj <asdfj@LAPTOP-RADBII5T>
2020-06-15 14:30:07 +0800

    1

--- seleniumWebdriverTest/src/main/java/Tests/Test1.java
+++ seleniumWebdriverTest/src/main/java/Tests/Test1.java
@@ -14,39 +14,49 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.Strs;
 
 public class Test1 {
-	public void runTheTest() {
-		System.setProperty("webdriver.chrome.driver", "D:\\chr\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	private WebDriver driver;
+	private WebElement function;
+	private WebElement action;
+	private WebElement userId;
+	private WebElement logBeginDate;
+	private WebElement logEndDate;
+	private WebElement form;
+	private WebElement searchButton;
+	private WebElement clearButton;
+	private WebElement printExcelButton;
+	private WebElement printPdfButton;
+	
+	
+	
+	/**
+	 * 預設登入後進使用者紀錄查詢
+	 */
+	public Test1() {
+		System.setProperty("webdriver.chrome.driver", "C:\\chr\\chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.get("http://10.0.0.123/MegaImage/home/SSOLogin?lightId=12345678");
 
-		WebElement s = driver.findElement(By.tagName("h5"));
-
+		WebElement s = driver.findElement(By.tagName("h5"));	
 		System.out.println("----------------------");
 		System.out.println(s);
 		System.out.println(s.getText());
 		System.out.println("----------------------");
-
-		// 點擊使用者紀錄查詢(id是SystemAdmin.TransactionLog)
+		// 找到系統管理
 		WebElement t = driver.findElement(By.partialLinkText("系統管理"));
-
 		System.out.println("----------------------");
 		System.out.println(t);
 		System.out.println(t.getText());
 		System.out.println("----------------------");
-
+		//點擊系統管理
 		t.click();
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+		sleep(1000);	
+		// 找到使用者紀錄查詢(id是SystemAdmin.TransactionLog)
 		WebElement href = driver.findElement(By.id("SystemAdmin.TransactionLog"));
 		System.out.println("----------------------");
 		System.out.println(href.getText());
@@ -54,52 +64,88 @@ public class Test1 {
 		System.out.println(href.getAttribute("href"));
 		System.out.println(href);
 		System.out.println("----------------------");
-
+		//點擊使用者紀錄
 		href.click();
+		
+		sleep(1000);
+		
+		function = driver.findElement(By.id("function"));
+		function = driver.findElement(By.id(""));
+		function = driver.findElement(By.id(""));
+		function = driver.findElement(By.id(""));
+		function = driver.findElement(By.id(""));
+		function = driver.findElement(By.id(""));
+		function = driver.findElement(By.id(""));
+		
+	}
+	
+	
+	public void put3Data(String function,String logBeginDate,String logEndDate) {
+//		System.setProperty("webdriver.chrome.driver", "C:\\chr\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
 
-//		driver.get("http://10.0.0.123/MegaImage/management/logFunctionManagePage");
-
-		List<WebElement> options = driver.findElements(By.tagName("option"));
-		for (WebElement option : options) {
-			String opt = option.getText();
-//			String opt = option.getAttribute("text");
-			System.out.println(opt);
-//			if ("Query.Image".equals(option.getAttribute("value"))) {
+//		List<WebElement> options = driver.findElements(By.tagName("option"));
+//		for (WebElement option : options) {
+//			String opt = option.getText();
+//			System.out.println(opt);
+//			if (function.equals(option.getAttribute("text"))) {
 //				option.click();
 //				break;
 //			}
-			if ("登入/登出".equals(option.getAttribute("text"))) {
-				option.click();
+//		}
+//		
+		Select select = new Select(driver.findElement(By.tagName("select")));
+		select.selectByVisibleText(function);
+
+		
+		sleep(1000);
+
+//		WebElement WElogBeginDate = driver.findElement(By.id("logBeginDate"));
+//		WElogBeginDate.clear();
+//		WElogBeginDate.sendKeys(logBeginDate);
+
+		this.logBeginDate.clear();
+		this.logBeginDate.sendKeys(logBeginDate);
+		
+		sleep(1000);
+
+		WebElement WElogEndDate = driver.findElement(By.id("logEndDate"));
+		WElogEndDate.clear();
+		WElogEndDate.sendKeys(logEndDate);
+		sleep(1000);
+		
+
+	}
+
+	public void putData(String id,String data) {
+		WebElement element = driver.findElement(By.id(id));
+		if(element == null) {
+			return;
+		}
+		element.clear();
+		element.sendKeys(data);
+	}
+	
+	public void quit() {
+		driver.quit();
+	}
+	
+	public void close() {
+		driver.close();
+	}
+	
+	public void printExcel() {
+		List<WebElement> buttons = driver.findElements(By.tagName("button"));
+		for (WebElement button : buttons) {
+			if ("匯出Excel".equals(button.getText())) {
+				button.click();
 				break;
 			}
 		}
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		WebElement logBeginDate = driver.findElement(By.id("logBeginDate"));
-		logBeginDate.clear();
-		logBeginDate.sendKeys("2020/06/11");
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		WebElement logEndDate = driver.findElement(By.id("logEndDate"));
-		logEndDate.clear();
-		logEndDate.sendKeys("2020/06/12");
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+		checkAlertMessage();
+	}
+	
+	public void search() {
 		List<WebElement> buttons = driver.findElements(By.tagName("button"));
 		for (WebElement button : buttons) {
 			if ("查詢".equals(button.getText())) {
@@ -107,9 +153,12 @@ public class Test1 {
 				break;
 			}
 		}
-
+		checkAlertMessage();
+	}
+	
+	private void checkAlertMessage() {
 		try {
-			(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+			(new WebDriverWait(driver, 1)).until(new ExpectedCondition<Boolean>() {
 	            public Boolean apply(WebDriver d) {
 	                return (d.findElement(By.className("bootbox-body")) != null );
 	            }
@@ -121,10 +170,12 @@ public class Test1 {
 		} catch (Exception e) {
 
 		}
+	}
+	
 
-		ArrayList<String> testResult = new ArrayList<String>();
+	public ArrayList<String> getResult(){
+		ArrayList<String> result = new ArrayList<String>();
 		StringBuffer headLine = new StringBuffer();
-
 		WebElement theadtr = driver.findElement(By.tagName("thead")).findElement(By.tagName("tr"));
 		List<WebElement> ths = theadtr.findElements(By.tagName("th"));
 		for (WebElement th : ths) {
@@ -132,12 +183,9 @@ public class Test1 {
 			headLine.append(tmp);
 			System.out.print(tmp);
 		}
-
-		testResult.add(headLine.toString());
+		result.add(headLine.toString());
 		System.out.println();
-
 		String clstring;
-		
 		do {
 			List<WebElement> trs = driver.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
 			for (WebElement tr : trs) {
@@ -148,7 +196,7 @@ public class Test1 {
 					bodyLine.append(tmp2);
 					System.out.print(tmp2);
 				}
-				testResult.add(bodyLine.toString());
+				result.add(bodyLine.toString());
 				System.out.println();
 			}
 			
@@ -162,20 +210,13 @@ public class Test1 {
 			}
 		}
 		while(true);
-		
-		saveText(testResult);
-		// 關閉瀏覽器
-		// driver.quit();
-		
-		System.out.println("over");
-
-	}
-
-	private void saveText(ArrayList<String> text, String path, String name) {
+		return result;
+	} 
+	
+	public void saveText(String path, String name) {
 		/* 寫入Txt檔案 */
-
+		ArrayList<String> text = getResult();
 		File writename = new File(path + "\\" + name + ".txt");
-
 		try {
 			writename.createNewFile();
 		} catch (IOException e) {
@@ -195,9 +236,133 @@ public class Test1 {
 		}
 
 	}
-
-	private void saveText(ArrayList<String> text) {
-		saveText(text, "D:\\text", "output");
+	
+	private void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void saveText() {
+		saveText("C:\\text", "output");
+	}
+
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+
+	public WebElement getFunction() {
+		return function;
+	}
+
+
+	public void setFunction(WebElement function) {
+		this.function = function;
+	}
+
+
+	public WebElement getAction() {
+		return action;
+	}
+
+
+	public void setAction(WebElement action) {
+		this.action = action;
+	}
+
+
+	public WebElement getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(WebElement userId) {
+		this.userId = userId;
+	}
+
+
+	public WebElement getLogBeginDate() {
+		return logBeginDate;
+	}
+
+
+	public void setLogBeginDate(WebElement logBeginDate) {
+		this.logBeginDate = logBeginDate;
+	}
+
+
+	public WebElement getLogEndDate() {
+		return logEndDate;
+	}
+
+
+	public void setLogEndDate(WebElement logEndDate) {
+		this.logEndDate = logEndDate;
+	}
+
+
+	public WebElement getForm() {
+		return form;
+	}
+
+
+	public void setForm(WebElement form) {
+		this.form = form;
+	}
+
+
+	public WebElement getSearchButton() {
+		return searchButton;
+	}
+
+
+	public void setSearchButton(WebElement searchButton) {
+		this.searchButton = searchButton;
+	}
+
+
+	public WebElement getClearButton() {
+		return clearButton;
+	}
+
+
+	public void setClearButton(WebElement clearButton) {
+		this.clearButton = clearButton;
+	}
+
+
+	public WebElement getPrintExcelButton() {
+		return printExcelButton;
+	}
+
+
+	public void setPrintExcelButton(WebElement printExcelButton) {
+		this.printExcelButton = printExcelButton;
+	}
+
+
+	public WebElement getPrintPdfButton() {
+		return printPdfButton;
+	}
+
+
+	public void setPrintPdfButton(WebElement printPdfButton) {
+		this.printPdfButton = printPdfButton;
+	}
+
+
+	public Select getFunctionTypeSelect() {
+		return new Select(this.function);
+	}
+
+	
 }
--- seleniumWebdriverTest/src/main/java/jack/seleniumWebdriverTest/App.java
+++ seleniumWebdriverTest/src/main/java/jack/seleniumWebdriverTest/App.java
@@ -1,6 +1,5 @@
 package jack.seleniumWebdriverTest;
 
-
 import java.io.*;
 import java.util.*;
 import org.openqa.selenium.By;
@@ -15,10 +14,21 @@ import utils.Strs;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	public static void main(String[] args) throws Exception {
 		Test1 t = new Test1();
-		t.runTheTest();
-    }
+		String function = "登入/登出";
+		String logBeginDate = "2020/06/10";
+		String logEndDate = "2020/06/10";
+		t.put3Data(function, logBeginDate, logEndDate);
+		
+		
+		
+//		t.putData("logBeginDate", "2020/03/11");
+		
+		t.search();
+//		t.printExcel();
+//		t.saveText();
+//		t.quit();
+	}
 }
